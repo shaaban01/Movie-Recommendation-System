bb4f55a755b2ba9614aec7be034813854fd8ed60
shaaban01 <mmc53@mail.aub.edu>
2023-06-22 09:49:31 +0300

    adding rating is functional

--- src/main.cpp
+++ src/main.cpp
@@ -13,30 +13,30 @@ int main()
     //     auth->authenticate();
     // }
 
-    std::vector<Movie> movies;
-    Movie movie;
-    std::unique_ptr<MovieApiController> movieController = std::make_unique<MovieApiController>();
+    // std::vector<Movie> movies;
+    // Movie movie;
+    // std::unique_ptr<MovieApiController> movieController = std::make_unique<MovieApiController>();
     // movieController->FetchMoviesByTitle("The Matrix", movies);
-    movieController->FetchMovieById("508602", movie);
-    movie.printAttributes();
-    movieController->StoreMovie("508602");
+    // movieController->FetchMovieById("508602", movie);
+    // movie.printAttributes();
+    // movieController->StoreMovie("508602");
     // std::unique_ptr<ViewMovies> viewer = std::make_unique<CmdViewMovies>(movies);
     // viewer->Display();
-    // std::unique_ptr<RatingController> ratingController = std::make_unique<RatingController>();
-    // if (ratingController->createRating(1, 385687, 8.5))
-    // {
-    //     std::cout << "Rating created successfully\n";
-    // }
-    // else
-    // {
-    //     std::cout << "Rating creation failed\n";
-    // }
+    std::unique_ptr<RatingController> ratingController = std::make_unique<RatingController>();
+    if (ratingController->createRating(1, 508602, 8.5))
+    {
+        std::cout << "Rating created successfully\n";
+    }
+    else
+    {
+        std::cout << "Rating creation failed\n";
+    }
 
-    // std::map<int, float> ratings = ratingController->getAllRatings(1);
+    std::map<int, float> ratings = ratingController->getAllRatings(1);
 
-    // for (const auto &elem : ratings)
-    // {
-    //     std::cout << elem.first << " " << elem.second << "\n";
-    // }
+    for (const auto &elem : ratings)
+    {
+        std::cout << elem.first << " " << elem.second << "\n";
+    }
     return 0;
 }
